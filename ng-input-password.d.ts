ff8d396d594e85076354362c72d2b0fd4de9dac2
= <=>
2017-08-10 15:23:41 +0100

    Fix V2

--- ng-input-password.d.ts
+++ ng-input-password.d.ts
@@ -1,15 +1,25 @@
 import { ControlValueAccessor } from '@angular/forms';
-export declare class InputPasswordComponent implements ControlValueAccessor {
+/**
+ * Generated class for the NgInputPasswordComponent component.
+ *
+ * See https://angular.io/docs/ts/latest/api/core/index/ComponentMetadata-class.html
+ * for more info on Angular Components.
+ */
+export declare class NgInputPasswordComponent implements ControlValueAccessor {
     placeholder: string;
     color: string;
     hideColor: string;
     icon: string;
+    label: string;
+    labelStyle: string;
     private isShow;
     private typeInput;
     constructor();
     private innerValue;
     private onTouchedCallback;
     private onChangeCallback;
+    private hasLabel();
+    private isLabelStyle(style);
     /**
      *
      */
